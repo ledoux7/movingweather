b2d38d9a55989908b213237f9356062a8598f2f9
ledoux7 <evensven1997@gmail.com>
2022-01-13 00:38:08 +0100

    Add charts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Dash from './components/Dash';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React123
-        </a>
-      </header>
+      <Dash />
     </div>
   );
 }
 
 export default App;
+
+// "snow.movinglines.io evensven1997@gmail.com"
